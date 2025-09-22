bc974b6c584612d6adf966d560c00ea48277a613
RafaelFett <rafaelfet@gmail.com>
2025-09-22 08:30:52 -0300

    wp

---
+++ js-algorithms-playground/src/pages/home/home.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+
+export default function HomePage() {
+
+
+  return (
+      <div style={{textAlign: 'center'}}>
+        <h1>Bem-vindo ao Playground de Algoritmos</h1>
+        <nav>
+          <ul>
+
+            <button><Link to="/algorithms">Algoritmos</Link></button>
+            <div className="divider"></div>
+
+            <button><Link to="/data-structures">Estruturas de Dados</Link></button>
+
+          </ul>
+        </nav>
+      </div>
+    
+  )
+}
